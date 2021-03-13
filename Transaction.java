3687f3be52258703c6257c518d907337307029b1
Conn O'Flynn <connoflynn@gmail.com>
2021-03-13 12:56:39 +0000

    init

--- Transaction.java
+++ Transaction.java
@@ -1,10 +1,11 @@
 import java.io.Serializable;
+import java.math.BigDecimal;
 import java.util.Date;
 
 public class Transaction extends Serializable 
 {
     // Needs some accessor methods to return information about the transaction
-    public Money getAmount();
+    public BigDecimal getAmount();
     public Date getDate();
     public String description;
 }
